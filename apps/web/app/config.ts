d767ec136f65a83bededb3da8345805a080d833e
gopal pamidimukkala <gopalpamidimukkala@gopals-MacBook-Pro.local>
2025-08-24 02:50:21 +0530

    rm space in config file

--- apps/web/app/config.ts
+++ apps/web/app/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://api.photo.pgopal.in ";
+export const BACKEND_URL = "https://api.photo.pgopal.in";
 export const CLOUDFLARE_URL = "https://pub-b10d2b916db5481c8406a32b7dd85221.r2.dev";
