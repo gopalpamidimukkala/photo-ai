e0a9ff34bfe62921c4c09d4f36d52ea2d0cf6348
gopal pamidimukkala <gopalpamidimukkala@gopals-MacBook-Pro.local>
2025-08-24 02:09:15 +0530

    log in the middleware

--- apps/web/app/config.ts
+++ apps/web/app/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:8080";
+export const BACKEND_URL = "https://api.photo.pgopal.in ";
 export const CLOUDFLARE_URL = "https://pub-b10d2b916db5481c8406a32b7dd85221.r2.dev";
