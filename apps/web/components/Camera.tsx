346c6dfc661a2ae80524a70fa81bc56e1c82c2a3
gopal pamidimukkala <gopalpamidimukkala@gopals-MacBook-Pro.local>
2025-08-24 03:09:51 +0530

    camera.tsx

--- apps/web/components/Camera.tsx
+++ apps/web/components/Camera.tsx
@@ -5,6 +5,10 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { ImageCard, ImageCardSkeleton, TImage } from "./ImageCard"
 
+type BulkResponse = {
+    images?: TImage[]
+}
+
 export function Camera() {
     const [images, setImages] = useState<TImage[]>([]);
     const [imagesLoading, setImagesLoading] = useState(true)
@@ -13,35 +17,33 @@ export function Camera() {
     useEffect(() => {
         (async() => {
             const token = await getToken()
-            const response = await axios.get(`${BACKEND_URL}/image/bulk`, {
+            const response = await axios.get<BulkResponse>(`${BACKEND_URL}/image/bulk`, {
                 headers: {
                     "Authorization": `Bearer ${token}`
                 }
             }
         );
-            setImages(response.data.images)
+            setImages(response.data.images || [])
             setImagesLoading(false)
         })()
     },[])
 
     useEffect(() => {
-        (async () => {
-            if (images.find(x => x.status !== "Generated")) {
-                await new Promise((r) => setTimeout(r, 5000));
-                const token = await getToken();
-                const response = await axios.get(`${BACKEND_URL}/image/bulk`, {
-                    headers: {
-                        "Authorization": `Bearer ${token}`
-                    }
-                })
-                setImages(response.data.images)
-            }
-        })()
-    },[images])
+        const interval = setInterval(async () => {
+          if (images.find(x => x.status !== "Generated")) {
+            const token = await getToken();
+            const response = await axios.get<BulkResponse>(`${BACKEND_URL}/image/bulk`, {
+              headers: { "Authorization": `Bearer ${token}` }
+            });
+            setImages(response.data.images || []);
+          }
+        }, 5000);
+        return () => clearInterval(interval);
+      }, [images]);      
 
     return (
         <div className="grid md:grid-cols-4 grid-cols-1 gap-2 pt-4">
-            { images.map(image => <ImageCard {...image} />)}
+            { images.map(image => <ImageCard key={image.id} {...image} />) }
             {imagesLoading && <> <ImageCardSkeleton></ImageCardSkeleton><ImageCardSkeleton></ImageCardSkeleton><ImageCardSkeleton></ImageCardSkeleton> <ImageCardSkeleton></ImageCardSkeleton> <ImageCardSkeleton></ImageCardSkeleton></> }
         </div>
     )
